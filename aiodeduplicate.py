d571e5b3f74bbe5c966d9713d2bdb44382619756
Michal Charemza <michal@charemza.name>
2019-03-31 10:28:18 +0100

    (refactor) One fewer dict lookup, one fewer return statement

--- aiodeduplicate.py
+++ aiodeduplicate.py
@@ -9,20 +9,20 @@ def deduplicate_concurrent(func):
     async def deduplicated(*args, **kwargs):
         identifier = (args, tuple(kwargs.items()))
 
-        if identifier in concurrent:
-            return await concurrent[identifier]
-        
-        future = asyncio.Future()
-        concurrent[identifier] = future
-
         try:
-            result = await func(*args, **kwargs)
-        except BaseException as exception:
-            future.set_exception(exception)
-        else:
-            future.set_result(result)
-        finally:
-            del concurrent[identifier]
+            future = concurrent[identifier]
+        except KeyError:
+            future = asyncio.Future()
+            concurrent[identifier] = future
+
+            try:
+                result = await func(*args, **kwargs)
+            except BaseException as exception:
+                future.set_exception(exception)
+            else:
+                future.set_result(result)
+            finally:
+                del concurrent[identifier]
 
         return await future
 
